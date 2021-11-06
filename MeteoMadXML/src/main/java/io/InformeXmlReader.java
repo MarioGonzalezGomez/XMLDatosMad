940c8e2d9d2f4f0785c9cbe66855c10dec717316
MarioGG <ggperpi@gmail.com>
2021-11-06 16:38:34 +0100

    Lector Xml ejercicio 2 con DOM

--- MeteoMadXML/src/main/java/io/InformeXmlReader.java
+++ MeteoMadXML/src/main/java/io/InformeXmlReader.java
@@ -1,22 +1,19 @@
 package io;
 
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPathExpressionException;
 import java.io.File;
 import java.io.IOException;
 
+
 public class InformeXmlReader {
 
-    public Document leerInforme() throws ParserConfigurationException, IOException, SAXException, XPathExpressionException {
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db = dbf.newDocumentBuilder();
-        Document d = db.parse(System.getProperty("user.dir") + File.separator + "src" + File.separator + "main" + File.separator
-                + "resources" + File.separator + "informes" + File.separator + "informe.xml");
+    public Document leerInforme() throws IOException, JDOMException {
+        SAXBuilder builder = new SAXBuilder();
+        Document d = builder.build(System.getProperty("user.dir") + File.separator + "src" + File.separator + "main" + File.separator
+                + "resources" + File.separator + "informes" + File.separator + "mediciones.xml");
         return d;
     }
 }
